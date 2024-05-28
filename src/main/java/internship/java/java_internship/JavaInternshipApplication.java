5544a3bcfd77fdd9dca584d082ba23c948e875da
wojciech.zieba <wojciech.zieba@jit.team>
2024-05-28 15:24:44 +0200

    INT-1 | initial commit for gradle and springboot project

---
+++ src/main/java/internship/java/java_internship/JavaInternshipApplication.java
@@ -0,0 +1,13 @@
+package internship.java.java_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/internship/java/java_internship/JavaInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package internship.java.java_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
